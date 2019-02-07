19c583cdcd940619d27ac8128ec31081b0efd14f
ernest-phillips <ernestlphillips@gmail.com>
2019-02-06 21:00:57 -0800

    corrected commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
+import "./App.css";
+
+function EnterGuess(props) {
+  //controlled forms react
+  return <input type="text">{props.text}</input>;
+}
+
+function SubmitGuess(props) {
+  return <button type="submit" />;
+}
+
+function PreviousGuess(props) {
+  return (
+    <div>
+      <ul>
+        <li />
+      </ul>
+    </div>
+  );
+}
+function NewGame() {
+  return <h3>New Game</h3>;
+}
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      temp: ["hot", "cold"],
+      enteredGuess: {},
+      guess: {},
+      guessNum: []
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Hot or Cold</h1>
+        <EnterGuess />
+        <SubmitGuess />
+        <PreviousGuess />
+        <NewGame />
       </div>
     );
   }
