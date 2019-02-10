791ab6a9b5a2e2a20d5ef545c7e37b4e2871094a
ernest-phillips <ernestlphillips@gmail.com>
2019-02-09 16:40:34 -0800

    new feature branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,53 @@
 import React, { Component } from "react";
+import Header from "./header";
+import GuessSection from "./guess-section";
+import StatusSection from "./info-section";
 
 import "./App.css";
+
+export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      guesses: [],
+      feedback: "Make your guess!",
+      auralStatus: "",
+      correctAnswer: Math.floor(Math.random() * 100) + 1
+    };
+  }
+
+  restartGame() {
+    this.setState({
+      guesses: [],
+      feedback: "Make your guess!",
+      auralStatus: "",
+      correctAnswer: Math.floor(Math.random() * 100) + 1
+    });
+  }
+
+  makeGues(guess) {
+    guess = parseInt(guess, 10);
+    if (isNaN(guess)) {
+      this.setState({ feedback: "Please enter a valid number" });
+      return;
+    }
+
+    const difference = Math.abs(guess - this.state.correctAnswer);
+
+    let feedback;
+    if (difference >= 50) {
+      feedback = "You're very COLD!";
+    } else if (difference >= 30) {
+      feedback = "You're cold!";
+    } else if (difference >= 10) {
+      feedback = "Getting Warm!";
+    } else if (difference >= 1) {
+      feedback = "Whoa you are HOT!";
+    } else {
+      feedback = "You Got It!";
+    }
+  }
+}
 
 function EnterGuess(props) {
   //controlled forms react
@@ -47,5 +94,3 @@ class App extends Component {
     );
   }
 }
-
-export default App;
